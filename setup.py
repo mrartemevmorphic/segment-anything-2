b192ae347da7c3dfec3ca284d4f93693886b1b58
mrartemevmorphic <158044304+mrartemevmorphic@users.noreply.github.com>
2024-08-13 15:45:10 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import os
 from setuptools import find_packages, setup
 
 # Package metadata
-NAME = "SAM 2"
+NAME = "SAM2"
 VERSION = "1.0"
 DESCRIPTION = "SAM 2: Segment Anything in Images and Videos"
 URL = "https://github.com/facebookresearch/segment-anything-2"
